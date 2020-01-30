2085a0f54d3ab2277006d97234e71e347e401299
Ashkan Lotfipoor <a.lotfipoor@gmail.com>
2020-01-30 10:55:45 +0000

    update

--- FileDescription.py
+++ FileDescription.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import xlrd
 
-path = 'C:/Users/al146/OneDrive - Heriot-Watt University/Data/Findhorn/Raw/'
+path = 'C:/Users/al146/OneDrive - Heriot-Watt University/Data/Findhorn/test/'
 
 # creats a list of all files with .csv format in above path.
 files = []
@@ -22,13 +22,13 @@ def mergeCSV(fileslist):
         table.loc[len(table)] = file
         filecsv = open(file)
         table['total_no'] = len(filecsv.readlines())
-        df = pd.read_csv(file, names=['time', 'meter'])
+        df = pd.read_csv(file, names=['date', 'time', 'meter'])
         table['zero_no'] = sum((df == 0).sum(axis=1))
         table['na_no'] = sum(pd.isnull(df['meter']))
         table['zero%'] = (table['zero_no']/table['total_no'])*100
         table['na%'] = (table['na_no']/table['total_no'])*100
         result = result.append(table, ignore_index=True)
     result.file_id.replace({'C:/Users/al146/OneDrive - Heriot-Watt University/Data/Findhorn/Raw/':''}, regex=True, inplace=True)
-    result.to_csv('result.csv')
+    result.to_csv('resulttest.csv')
 
 mergeCSV(files)
